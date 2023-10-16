61cdda8383b9e5d49a3cbf8e5ff3ab8910240cab
ThinhHV <thinh@thinhhv.com>
2023-10-16 18:46:01 +0000

    Add trade stats
    Signed-off-by: ThinhHV <thinh@thinhhv.com>

--- src/modules/stats/stats.service.ts
+++ src/modules/stats/stats.service.ts
@@ -17,21 +17,19 @@ export class StatsService {
 
     // stats data
     const statsGql = readFile("./graphql/stats.gql", __dirname);
-    const data: any = await request<any>(graphql.uri, statsGql, {
-      timestamp_start: start,
-      timestamp_end: end,
-      timestamp_24h: getLinuxTimestampBefore24Hours(),
-    }).catch((error) => {
-      console.error(error);
-      return {} as any;
-    });
-
-    // calc overview
-    const dataPayoutCalc = this.calcOverview(data.tradingStatsOverview);
+    const { userStats, USDCstats, USDC24stats, tradingStatsOverview, tradingStats, volumeStats }: any =
+      await request<any>(graphql.uri, statsGql, {
+        timestamp_start: start,
+        timestamp_end: end,
+        timestamp_24h: getLinuxTimestampBefore24Hours(),
+      }).catch((error) => {
+        console.error(error);
+        return {} as any;
+      });
 
     // fees
     const USDC24hrsStats = {
-      ...data.USDC24stats.reduce(
+      ...USDC24stats?.reduce(
         (acc, curr) => {
           return {
             amount: +acc.amount + +curr.amount,
@@ -42,19 +40,41 @@ export class StatsService {
       ),
     };
     // user
-    const usersData = data.userStats;
+    const usersData = (() => {
+      let cumulativeNewUserCount = 0;
+      return userStats
+        ? sortBy(userStats, "timestamp").map((item) => {
+            cumulativeNewUserCount += item.uniqueCount;
+            const oldCount = item.existingCount;
+            const totalCount = item.uniqueCount + oldCount;
+            const oldPercent = ((oldCount / totalCount) * 100).toFixed(1);
+            return {
+              all: item.uniqueCount + oldCount,
+              newCount: item.uniqueCount,
+              uniqueSum: 0,
+              oldCount,
+              oldPercent,
+              cumulativeNewUserCount,
+              ...item,
+              timestamp: item.timestamp,
+            };
+          })
+        : [];
+    })();
     const totalUsers = usersData[usersData.length - 1]?.cumulativeNewUserCount;
     const prevTotalUsers = usersData[usersData.length - 2]?.cumulativeNewUserCount;
     const totalUsersDelta = totalUsers && prevTotalUsers ? totalUsers - prevTotalUsers : null;
     // payout
+    const dataPayoutCalc = this.calcPayoutOverview(tradingStatsOverview);
     const payout = dataPayoutCalc?.data[dataPayoutCalc?.data.length - 1]?.pnlCumulative || 0;
     const payoutDelta = payout - (dataPayoutCalc?.data[dataPayoutCalc?.data.length - 2]?.pnlCumulative || 0);
+
     // res overview
     const dataOverview = {
-      totalVolume: data.USDCstats.totalVolume / 1e6,
-      totalVolumeDelta: USDC24hrsStats.amount,
-      totalFees: data.USDCstats.totalSettlementFees / 1e6,
-      totalFeesDelta: USDC24hrsStats.settlementFee,
+      totalVolume: USDCstats?.totalVolume / 1e6,
+      totalVolumeDelta: USDC24hrsStats?.amount,
+      totalFees: USDCstats?.totalSettlementFees / 1e6,
+      totalFeesDelta: USDC24hrsStats?.settlementFee,
       totalUsers,
       totalUsersDelta,
       // openInterest: data.dashboardStats?.[0].openInterest / 1e6,
@@ -84,14 +104,16 @@ export class StatsService {
     // calc pool
 
     return {
-      ...data,
-      overviewStats: dataOverview,
+      USDC24stats,
+      volumeStats,
       burnedBFRs: dataBurned?.burnedBFRs || [],
+      overviewStats: dataOverview,
       poolStats: this.calcPoolStats(dataPool || { poolStats: [] }),
+      tradingStats: this.calcTradersData(tradingStats),
     } as any;
   }
 
-  private calcOverview(_data: any) {
+  private calcTradersData(_data: any) {
     let currentPnlCumulative = 0;
     let currentProfitCumulative = 0;
     let currentLossCumulative = 0;
@@ -99,10 +121,10 @@ export class StatsService {
     const data =
       _data && _data.length
         ? sortBy(_data, (i) => i.timestamp).map((dataItem) => {
-            const profit = +dataItem.profitBFR / 1e6;
-            const loss = +dataItem.lossBFR / 1e6;
-            const profitCumulative = +dataItem.profitCumulativeBFR / 1e6;
-            const lossCumulative = +dataItem.lossCumulativeBFR / 1e6;
+            const profit = +dataItem.profitUSDC / 1e6;
+            const loss = +dataItem.lossUSDC / 1e6;
+            const profitCumulative = +dataItem.profitCumulativeUSDC / 1e6;
+            const lossCumulative = +dataItem.lossCumulativeUSDC / 1e6;
             const pnlCumulative = profitCumulative - lossCumulative;
             const pnl = profit - loss;
             currentProfitCumulative += profit;
@@ -124,6 +146,71 @@ export class StatsService {
         : null;
 
     if (data) {
+      // console.log(data,'data')
+      const maxProfit = maxBy(data, (item) => item.profit)?.profit || 0;
+      const maxLoss = minBy(data, (item) => item.loss)?.loss || 0;
+      const maxProfitLoss = Math.max(maxProfit, -maxLoss);
+
+      const maxPnl = maxBy(data, (item) => item.pnl)?.pnl || 0;
+      const minPnl = minBy(data, (item) => item.pnl)?.pnl || 0;
+      const maxCurrentCumulativePnl = maxBy(data, (item) => item.currentPnlCumulative)?.currentPnlCumulative || 0;
+      const minCurrentCumulativePnl = minBy(data, (item) => item.currentPnlCumulative)?.currentPnlCumulative || 0;
+
+      const currentProfitCumulative = data[data.length - 1].currentProfitCumulative;
+      const currentLossCumulative = data[data.length - 1].currentLossCumulative;
+      const stats = {
+        maxProfit,
+        maxLoss,
+        maxProfitLoss,
+        currentProfitCumulative,
+        currentLossCumulative,
+        maxCurrentCumulativeProfitLoss: Math.max(currentProfitCumulative, -currentLossCumulative),
+
+        maxAbsPnl: Math.max(Math.abs(maxPnl), Math.abs(minPnl)),
+        maxAbsCumulativePnl: Math.max(Math.abs(maxCurrentCumulativePnl), Math.abs(minCurrentCumulativePnl)),
+      };
+
+      return {
+        data,
+        stats,
+      };
+    }
+  }
+
+  private calcPayoutOverview(_data: any) {
+    let currentPnlCumulative = 0;
+    let currentProfitCumulative = 0;
+    let currentLossCumulative = 0;
+
+    const data =
+      _data && _data.length
+        ? sortBy(_data, (i) => i.timestamp).map((dataItem) => {
+            const profit = +dataItem.profit / 1e6;
+            const loss = +dataItem.loss / 1e6;
+            const profitCumulative = +dataItem.profitCumulative / 1e6;
+            const lossCumulative = +dataItem.lossCumulative / 1e6;
+            const pnlCumulative = profitCumulative - lossCumulative;
+            const pnl = profit - loss;
+            currentProfitCumulative += profit;
+            currentLossCumulative -= loss;
+            currentPnlCumulative += pnl;
+            return {
+              profit,
+              loss: -loss,
+              profitCumulative,
+              lossCumulative: -lossCumulative,
+              pnl,
+              pnlCumulative,
+              timestamp: dataItem.timestamp,
+              currentPnlCumulative,
+              currentLossCumulative,
+              currentProfitCumulative,
+            };
+          })
+        : null;
+
+    if (data) {
+      // console.log(data,'data')
       const maxProfit = maxBy(data, (item) => item.profit)?.profit || 0;
       const maxLoss = minBy(data, (item) => item.loss)?.loss || 0;
       const maxProfitLoss = Math.max(maxProfit, -maxLoss);
