279f510c391ebf486e9de6b6cf8f1cff01533ac8
ThinhHV <thinh@thinhhv.com>
2023-10-25 17:36:18 +0000

    FIx build
    Signed-off-by: ThinhHV <thinh@thinhhv.com>

--- src/modules/trades/trades.module.ts
+++ src/modules/trades/trades.module.ts
@@ -5,11 +5,12 @@ import { TRADES_MODEL, TradesSchema } from "./schemas/trades.schema";
 import { TradesController } from "./trades.controller";
 import { TradesService } from "./trades.service";
 import { UsersModule } from "modules/users/users.module";
+import { JobTradeService } from "modules/_jobs/trades/job-trade.service";
 
 @Module({
   imports: [forwardRef(() => UsersModule), MongooseModule.forFeature([{ name: TRADES_MODEL, schema: TradesSchema }])],
   controllers: [TradesController],
-  providers: [TradesService],
-  exports: [TradesService],
+  providers: [TradesService, JobTradeService],
+  exports: [TradesService, JobTradeService],
 })
 export class TradesModule {}
