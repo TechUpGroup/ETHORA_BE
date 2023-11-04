22b789febe92dd8651f50bc6fed74936d51093bf
moderate301297 <truongluat3012@gmal.com>
2023-11-04 11:19:25 +0700

    turn off job sync block

--- src/modules/_jobs/job.module.ts
+++ src/modules/_jobs/job.module.ts
@@ -2,9 +2,9 @@ import { Module } from "@nestjs/common";
 
 import { SyncEventModule } from "./sync-event/sync-event.module";
 import { JobTradeModule } from "./trades/job-trade.module";
-import { SyncBlockModule } from "./sync-block/sync-block.module";
+// import { SyncBlockModule } from "./sync-block/sync-block.module";
 
 @Module({
-  imports: [SyncEventModule, JobTradeModule, SyncBlockModule],
+  imports: [SyncEventModule, JobTradeModule],
 })
 export class JobModule {}
